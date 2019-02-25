6b69d3667c659295262cb972b87fdf789a6b64da
Lukas Jatmiko <lukas@ainosi.co.id>
2019-02-25 15:37:25 +0700

    add handler for int64 and float64

--- jsont_test.go
+++ jsont_test.go
@@ -1,6 +1,7 @@
 package jsont
 
 import (
+	"regexp"
 	"testing"
 )
 
@@ -17,9 +18,29 @@ func TestToJSON(t *testing.T) {
 	ar = append(ar, e)
 	ar = append(ar, g)
 	a["b"] = ar
+	var j int64
+	j = 100
+	a["i"] = j
+	a["k"] = 2.00
+	a["l"] = nil
 
 	rslt := ToJSON(a)
-	if rslt != `{"b":[{"d":1},{"f":"Success"},{"h":true}]}` {
-		t.Errorf("JSON Output is not match (%v)", rslt)
+
+	// {"b":[{"d":1},{"f":"Success"},{"h":true}],"i":100,"k":2.00,"l":""}
+	rgx := regexp.MustCompile(`(?i)\"b\"\:\[\{\"d\"\:1\}\,\{\"f\"\:\"Success\"}\,\{\"h\"\:true\}\]`)
+	if !rgx.Match([]byte(rslt)) {
+		t.Errorf("JSON Output is not match (%v)", `"b":[{"d":1},{"f":"Success"},{"h":true}]`)
+	}
+	rgx = regexp.MustCompile(`(?i)\"i\"\:100`)
+	if !rgx.Match([]byte(rslt)) {
+		t.Errorf("JSON Output is not match (%v)", `"i":100`)
+	}
+	rgx = regexp.MustCompile(`(?i)\"k\"\:2\.00`)
+	if !rgx.Match([]byte(rslt)) {
+		t.Errorf("JSON Output is not match (%v)", `"k":2.00`)
+	}
+	rgx = regexp.MustCompile(`(?i)\"l\"\:\"\"`)
+	if !rgx.Match([]byte(rslt)) {
+		t.Errorf("JSON Output is not match (%v)", `"l":""`)
 	}
 }
